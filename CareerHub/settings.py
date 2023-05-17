9e515033d760353f69ce341d9a6ff7ddbf2b29db
asxkzk <wjr0814@gmail.com>
2023-05-17 20:07:37 +0800

    test2

--- CareerHub/settings.py
+++ CareerHub/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'delegation'
 ]
 
 MIDDLEWARE = [
@@ -79,7 +80,7 @@ DATABASES = {
         # "ENGINE": "django.db.backends.sqlite3",
         # "NAME": BASE_DIR / "db.sqlite3",
         "ENGINE":'django.db.backends.mysql',
-        "NAME":'CareeHubInfo',
+        "NAME":'careehubinfo',
         "USER":'root',
         "PASSWORD":"abc123",
         "HOST":"127.0.0.1",
